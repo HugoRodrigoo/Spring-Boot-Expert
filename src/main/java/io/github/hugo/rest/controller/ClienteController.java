1984af89cbe53c0f0b301bbb6bb59d6bfdfb6b1b
HugoRodrigo <hugorbarradas@gmail.com>
2022-07-10 13:59:50 -0300

    Delete Mapping

--- src/main/java/io/github/hugo/rest/controller/ClienteController.java
+++ src/main/java/io/github/hugo/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.hugo.rest.controller;
 
 import io.github.hugo.domain.entity.Cliente;
 import io.github.hugo.domain.repository.Clientes;
+import net.bytebuddy.asm.Advice;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +35,19 @@ public class ClienteController {
     public ResponseEntity save( @RequestBody Cliente cliente){
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
+    }
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public  ResponseEntity delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+        if(cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+
     }
 
 }
